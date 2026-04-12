f038da4e5ed2a2e0c4a14ca841b5fe6bfb3706c1
kertas <kertas@kertass-MacBook-Air.local>
2026-04-12 17:40:10 +0700

    fix: sort year di report

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -199,7 +199,7 @@ class ReportController extends Controller
     {
         // $earliest = Order::min(DB::raw('strftime(\'%Y\', created_at)')) ?? now()->year;
         $oldestOrderDate = Order::whereNull('deleted_at')->min('created_at');
-        $earliest = $oldestOrderDate ? (int) $oldestOrderDate->format('Y') : now()->year;
+        $earliest = $oldestOrderDate ? \Carbon\Carbon::parse($oldestOrderDate)->format('Y') : now()->year;
 
         return range((int) $earliest, now()->year);
     }
